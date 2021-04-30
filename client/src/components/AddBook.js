6a548390c389fbd5e61108a79d7882b6b3fdccb6
Hamdi <hamdi.rahal@gmail.com>
2021-04-30 11:28:53 +0100

    handle form

--- client/src/components/AddBook.js
+++ client/src/components/AddBook.js
@@ -1,53 +1,53 @@
-import React from 'react';
-import { useQuery, gql } from "@apollo/client";
+import React, { useState } from "react";
+import { useQuery } from "@apollo/client";
+import { getAuthorsQuery } from "../queries/queries";
 
-const getAuthorsQuery = gql`
-    {
-        authors {
-            name
-            id
-        }
-    }
-`;
+const AddBook = () => {
+  const [form, setform] = useState({});
+  const { loading, error, data } = useQuery(getAuthorsQuery);
 
-const AddBook =()=> {
-    const { loading, error, data } = useQuery(getAuthorsQuery);
+  if (error) return <p>Error :(</p>;
 
-    if (error) return <p>Error :(</p>;
-
-    const displayAuthors=()=>{
-   
-        if(loading){
-            return( <option disabled>Loading authors</option> );
-        } else {
-            return data.authors.map(author => {
-                return( <option key={ author.id } value={author.id}>{ author.name }</option> );
-            });
-        }
-    }
- 
-        return(
-            <form id="add-book">
-                <div className="field">
-                    <label>Book name:</label>
-                    <input type="text" />
-                </div>
-                <div className="field">
-                    <label>Genre:</label>
-                    <input type="text" />
-                </div>
-                <div className="field">
-                    <label>Author:</label>
-                    <select>
-                        <option>Select author</option>
-                        { displayAuthors() }
-                    </select>
-                </div>
-                <button>+</button>
-
-            </form>
+  const displayAuthors = () => {
+    if (loading) {
+      return <option disabled>Loading authors</option>;
+    } else {
+      return data.authors.map((author) => {
+        return (
+          <option key={author.id} value={author.id}>
+            {author.name}
+          </option>
         );
-    
-}
+      });
+    }
+  };
+  const handleChange = (e) => {
+    setform({ ...form, [e.target.name]: e.target.value });
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(form);
+  };
+  return (
+    <form id="add-book" onSubmit={handleSubmit}>
+      <div className="field">
+        <label>Book name:</label>
+        <input type="text" name="name" onChange={handleChange} />
+      </div>
+      <div className="field">
+        <label>Genre:</label>
+        <input type="text" name="genre" onChange={handleChange} />
+      </div>
+      <div className="field">
+        <label>Author:</label>
+        <select name="authorId" onChange={handleChange}>
+          <option>Select author</option>
+          {displayAuthors()}
+        </select>
+      </div>
+      <button>+</button>
+    </form>
+  );
+};
 
-export default AddBook
+export default AddBook;
