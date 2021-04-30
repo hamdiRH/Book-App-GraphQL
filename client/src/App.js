6ac59aef7ab6d5299b81b4f9225dcbe088d5b911
Hamdi <hamdi.rahal@gmail.com>
2021-04-30 11:09:00 +0100

    AddBook

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,15 @@
-import React, { Component } from "react";
+import React from "react";
 
 // components
 import BookList from "./components/BookList";
+import AddBook from "./components/AddBook";
 
 const App = () => {
   return (
     <div id="main">
       <h1>Ninja's Reading List</h1>
       <BookList />
+      <AddBook />
     </div>
   );
 };
